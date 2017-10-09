c572157a66511074ad9ed5a1d4fda46fe151fa06
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-10-09 17:09:32 -0500

    attempt no-sandbox as chrome doesn't run well as root
    AMO BKIT-24

--- test/end2end/features/support/world.js
+++ test/end2end/features/support/world.js
@@ -1,11 +1,15 @@
 require('chromedriver')
 const { Builder, logging } = require('selenium-webdriver')
+const chrome = require('selenium-webdriver/chrome')
 const {defineSupportCode} = require('cucumber')
 
 function CustomWorld() {
   logging.installConsoleHandler()
+  const options = new chrome.Options();
+  options.addArguments('no-sandbox');
   this.driver = new Builder()
     .forBrowser('chrome')
+    .setChromeOptions(options)
     .build()
 }
 
