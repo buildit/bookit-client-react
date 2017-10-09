698944118cba98af1eb390297021baa4f4c35f8b
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-10-09 14:07:29 -0500

    Honor ENDPOINT_URI when running e2e tests
    AMO EAS BKIT-24

--- test/end2end/features/step-definitions/booking-steps.js
+++ test/end2end/features/step-definitions/booking-steps.js
@@ -2,9 +2,12 @@ const {defineSupportCode} = require('cucumber')
 const {By} = require('selenium-webdriver')
 const seleniumWebdriver = require('selenium-webdriver')
 
+const url = process.env.ENDPOINT_URI || 'http://localhost:3001'
+
 defineSupportCode(function({Given, When, Then}) {
   Given('I am on the bookit website', function () {
-    return this.driver.get('http://localhost:3001')
+
+    return this.driver.get(url)
   })
 
   When('I book a room', function () {
