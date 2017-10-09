0f651268d97760bb37dcdb9fbb248e6500c4f42d
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-10-09 13:55:54 -0500

    Lint all the things!
    AMO EAS BKIT-24

--- test/__mocks__/storageMock.js
+++ test/__mocks__/storageMock.js
@@ -1,19 +1,19 @@
 const storageMock = () => {
-  let store = {};
+  let store = {}
   return {
     getItem: key => store[key],
-    setItem: (key, value) => { store[key] = value.toString(); },
-    removeItem: (key) => { delete store[key]; },
-    clear: () => { store = {}; },
-  };
-};
+    setItem: (key, value) => { store[key] = value.toString() },
+    removeItem: (key) => { delete store[key] },
+    clear: () => { store = {} },
+  }
+}
 
-const localStorageMock = (() => storageMock())();
+const localStorageMock = (() => storageMock())()
 
-global.localStorage = localStorageMock;
+global.localStorage = localStorageMock
 
 Object.defineProperty(
   window,
   'localStorage',
   { value: localStorageMock, configurable: true, enumerable: true, writable: true }
-);
+)
--- test/end2end/features/step-definitions/booking-steps.js
+++ test/end2end/features/step-definitions/booking-steps.js
@@ -1,19 +1,19 @@
-var {defineSupportCode} = require('cucumber');
-var {By, until, Key} = require('selenium-webdriver');
-var seleniumWebdriver = require('selenium-webdriver');
+var {defineSupportCode} = require('cucumber')
+var {By} = require('selenium-webdriver')
+var seleniumWebdriver = require('selenium-webdriver')
 
 defineSupportCode(function({Given, When, Then}) {
-    Given('I am on the bookit website', function () {
-        return this.driver.get('http://localhost:3001');
-    });
+  Given('I am on the bookit website', function () {
+    return this.driver.get('http://localhost:3001')
+  })
 
-    When('I book a room', function () {
-        this.driver.findElement(By.id('bookit')).click();
-    });
+  When('I book a room', function () {
+    this.driver.findElement(By.id('bookit')).click()
+  })
 
-    Then('It\'s booked', function () {
-        var xpath = "//*[contains(text(),'Booked!')]";
-        var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
-        return this.driver.wait(condition, 2000);
-    });
-});
+  Then('It\'s booked', function () {
+    var xpath = "//*[contains(text(),'Booked!')]"
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath})
+    return this.driver.wait(condition, 2000)
+  })
+})
