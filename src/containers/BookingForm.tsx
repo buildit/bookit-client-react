a8de0ba02484ce0c1165108f34ebf3f9b9200639
Lizzie Szoke <elizabeth.szoke@wipro.com>
2017-10-30 11:44:30 -0600

    add validation

--- src/containers/BookingForm.tsx
+++ src/containers/BookingForm.tsx
@@ -26,6 +26,26 @@ interface BookingFormProps {
 
 type AllBookingFormProps = BookingFormProps & InjectedFormProps<BookingFormData>
 
+const required = (value) => (value ? undefined : 'Required')
+
+const endAfterStart = (value, {start}) => {
+  try {
+    if (Moment(value).isBefore(start)) {
+      return 'End must be after start'
+    }
+  }
+  catch (error) { } // tslint:disable-line
+}
+
+const startBeforeEnd = (value, {end}) => {
+  try {
+    if (Moment(value).isAfter(end)) {
+      return 'Start must be before end'
+    }
+  }
+  catch (error) { } // tslint:disable-line  
+}
+
 const renderField = ({
   input,
   label,
@@ -46,17 +66,16 @@ const renderField = ({
 const renderSuccessMessage = (bookingId) => <h1>Booking Created with booking ID {bookingId}!</h1>
 
 export const BookingForm: React.SFC<AllBookingFormProps> = (props) => {
-  const { handleSubmit, createBooking, submitting, bookingInstanceId } = props
+  const { handleSubmit, createBooking, submitting, bookingInstanceId, pristine, invalid } = props
 
   return (
     <div>
       <form onSubmit={ handleSubmit(createBooking) }>
         <Field name="bookableId" component={ renderField } type="hidden" label="Name of Room" />
-        <Field name="subject" component={ renderField } label="My Booking" type="text" />
-        <Field name="start" component={ renderField } label="Start" type="text" />
-        <Field name="end" component={ renderField } label="End" type="text" />
-        <Button type="submit" disabled={ submitting } id="bookit">
-
+        <Field name="subject" component={ renderField } label="My Booking" type="text" validate={required} />
+        <Field name="start" component={ renderField } label="Start" type="text" validate={[required, startBeforeEnd]} />
+        <Field name="end" component={ renderField } label="End" type="text" validate={[required, endAfterStart]} />
+        <Button type="submit" disabled={ pristine || submitting || invalid } id="bookit">
           Book a Room!
         </Button>
       </form>
