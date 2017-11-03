74262e686436a9f65c4cd7de2068f9fe76dbc743
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-11-03 15:19:31 -0500

    ES6 fat arrow and async/await with selenium

--- test/end2end/features/step-definitions/landing-page.js
+++ test/end2end/features/step-definitions/landing-page.js
@@ -1,16 +1,17 @@
-const { defineSupportCode } = require('cucumber')
-const { By } = require('selenium-webdriver')
+import { defineSupportCode } from 'cucumber'
+import { By } from 'selenium-webdriver'
+import { driver, url } from '../support/hooks'
 
-defineSupportCode(function({Given, When, Then}) {
-  Given('I am on the landing page of Bookit', function () {
-    return this.driver.get(this.url)
+defineSupportCode(({ Given, When, Then }) => {
+  Given('I am on the landing page of Bookit', async () => {
+    await driver.get(url)
   })
 
-  When('I click the Book a Room button', function () {
-    this.driver.findElement(By.tagName('button')).click()
+  When('I click the Book a Room button', async () => {
+    await driver.findElement(By.tagName('button')).click()
   })
 
-  Then('I am on the booking form', function () {
-    return this.driver.findElement(By.tagName('form'))
+  Then('I am on the booking form', async () => {
+    await driver.findElement(By.tagName('form'))
   })
 })
--- test/end2end/features/support/world.js
+++ test/end2end/features/support/world.js
@@ -1,20 +1,5 @@
-require('chromedriver')
-const { Builder, logging } = require('selenium-webdriver')
-const chrome = require('selenium-webdriver/chrome')
-const {defineSupportCode} = require('cucumber')
+import {defineSupportCode} from 'cucumber'
 
-function CustomWorld() {
-  logging.installConsoleHandler()
-  const options = new chrome.Options()
-  options.addArguments('no-sandbox')
-  this.driver = new Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(options)
-    .build()
-  this.url = process.env.ENDPOINT_URI || 'http://localhost:3001'
-}
-
-defineSupportCode(function({setWorldConstructor, setDefaultTimeout}) {
+defineSupportCode(({setDefaultTimeout}) => {
   setDefaultTimeout(5 * 60 * 1000)
-  setWorldConstructor(CustomWorld)
 })
