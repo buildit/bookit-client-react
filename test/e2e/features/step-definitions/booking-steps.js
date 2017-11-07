4789f236cf916edd768aefea0f6b6519887d90fe
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-11-06 19:16:49 -0600

    more super explicit fun
    guessing a race condition on the intial state setting the initial values of hte intputs

--- test/e2e/features/step-definitions/booking-steps.js
+++ test/e2e/features/step-definitions/booking-steps.js
@@ -8,23 +8,21 @@ defineSupportCode(({Given, When, Then}) => {
     await driver.get(`${url}/book`)
   })
 
-  When('I book a room', async function() {
-    let screenshot = await driver.takeScreenshot()
-    this.attach(screenshot, 'image/png')
+  When('I book a room', async () => {
     const start = faker.date.future(2)
     const end = new Date(start)
     end.setMinutes(start.getMinutes() + 1)
     const startForForm = start.toISOString().split('.')[0]
     const endForForm = end.toISOString().split('.')[0]
-    const startInput = await driver.findElement(By.name('start'))
-    await startInput.clear()
-    const endInput = await driver.findElement(By.name('end'))
-    await endInput.clear()
     const subjectInput = await driver.findElement(By.name('subject'))
-    screenshot = await driver.takeScreenshot()
-    this.attach(screenshot, 'image/png')
     await subjectInput.sendKeys('My Bookable')
+    const startInput = await driver.findElement(By.name('start'))
+    await driver.wait(async () => (await startInput.getAttribute('value')).includes('T'))
+    await startInput.clear()
     await startInput.sendKeys(startForForm)
+    const endInput = await driver.findElement(By.name('end'))
+    await driver.wait(async () => (await endInput.getAttribute('value')).includes('T'))
+    await endInput.clear()
     await endInput.sendKeys(endForForm)
     const createButton = await driver.findElement(By.tagName('button'))
     await createButton.click()
