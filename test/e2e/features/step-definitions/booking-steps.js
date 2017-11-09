294d56738e2ddba25e1604ffecf390b78cea197e
Lizzie Szoke <elizabeth.szoke@wipro.com>
2017-11-09 09:31:27 -0700

    cucumber fixes

--- test/e2e/features/step-definitions/booking-steps.js
+++ test/e2e/features/step-definitions/booking-steps.js
@@ -61,29 +61,6 @@ defineSupportCode(({ Given, When, Then, Before }) => {
     console.log('HAPPENING AGAIN')
   })
 
-  // When('I try to book the room again', () => {
-  //   driver.navigate().refresh()
-  //   const end = new Date(start)
-  //   end.setMinutes(start.getMinutes() + 1)
-  //   const startForForm = start.toISOString().split('.')[0]
-  //   const endForForm = end.toISOString().split('.')[0]
-  //   const subjectInput = awa driver.findElement(By.name('subject'))
-  //   await subjectInput.sendKeys('My Bookable')
-  //   const startInput = await driver.findElement(By.name('start'))
-  //   await driver.wait(async () => (await startInput.getAttribute('value')).includes('T'))
-  //   await startInput.clear()
-  //   await startInput.sendKeys(startForForm)
-  //   const endInput = await driver.findElement(By.name('end'))
-  //   await driver.wait(async () => (await endInput.getAttribute('value')).includes('T'))
-  //   await endInput.clear()
-  //   await endInput.sendKeys(endForForm)
-  //   console.log('start', startForForm)
-  //   console.log('end', endForForm)
-  //   const createButton = await driver.findElement(By.tagName('button'))
-  //   await createButton.click()
-  //   console.log('HAPPENING')
-  // })
-
   Then('It\'s booked', async () => {
     const condition = until.elementLocated(By.tagName('h1'))
     const element = await driver.wait(condition)
