21120485c74a0ef8d3d406c4beb1446eddf007d9
Chris Ashurst <christopher.ashurst@wipro.com>
2017-11-14 14:57:48 -0500

    updated tests

--- src/containers/BookingsList.test.jsx
+++ src/containers/BookingsList.test.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import BookingsList from 'Containers/BookingsList'
+import { BookingsList } from 'Containers/BookingsList'
 
 describe('<BookingsList />', () => {
   it('renders itself with a heading', () => {
-    const wrapper = shallow(<BookingsList />)
+    const wrapper = shallow(<BookingsList bookingDates={['2017-11-12', '2017-11-13']} />)
     expect(wrapper.find('h2')).to.exist
   })
 })
