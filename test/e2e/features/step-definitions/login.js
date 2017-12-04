159e7d6052d2e921e280ef53aabdbf94ac4dcaf8
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-12-04 17:22:46 -0600

    wait until button is visible?
    BKIT-118

--- test/e2e/features/step-definitions/login.js
+++ test/e2e/features/step-definitions/login.js
@@ -10,6 +10,7 @@ Given('I am on the login page in testing', async function() {
 When('I click the login button', async function() {
   const condition = until.elementLocated(By.id('login'))
   const element = await this.driver.wait(condition)
+  await this.driver.wait(until.elementIsVisible(element))
   await element.click()
 })
 
