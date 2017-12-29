980d172b178002e4606e71dce3733c61fe81b508
Chris Ashurst <christopher.ashurst@wipro.com>
2017-12-29 17:29:16 -0500

    XXX: add a bunch of cucumber/selenium helpers on World to reduce code verbosity in step definition modules

--- test/e2e/features/step-definitions/cancel.js
+++ test/e2e/features/step-definitions/cancel.js
@@ -1,60 +1,26 @@
 import { Given, When, Then} from 'cucumber'
 import { By } from 'selenium-webdriver'
 
-import addWeeks from 'date-fns/add_weeks'
-
 Given('I book a room', async function() {
   await this.getWithLogin('/book')
 
-  const locationInput = await this.waitUntilElement(By.name('locationId'))
-  await locationInput.sendKeys('NYC')
-
-  const start = addWeeks(this.start, 1)
-  const end = new Date(start)
-  end.setMinutes(start.getMinutes() + 1)
-
-  const startForForm = start.toISOString().split('.')[0]
-  const endForForm = end.toISOString().split('.')[0]
-
-  const subjectInput = await this.waitUntilElement(By.name('subject'))
-  await subjectInput.sendKeys('My Bookable To Be Deleted')
-
-  const startInput = await this.findElementByName('start')
-  await this.driver.wait(async () => (await startInput.getAttribute('value')).includes('T'))
-  await startInput.clear()
-  await startInput.sendKeys(startForForm)
-
-  const endInput = await this.findElementByName('end')
-  await this.driver.wait(async () => (await endInput.getAttribute('value')).includes('T'))
-  await endInput.clear()
-  await endInput.sendKeys(endForForm)
-
-  await this.findElementByLinkText('Rooms').click()
-
-  await this.driver.sleep(2000)
-
-  const element = await this.waitUntilElement(By.xpath('//h2[contains(text(),"Black Room")]'))
-  await element.click()
-
-  await this.driver.sleep(2000)
-
-  const createButton = await this.findElementByTagName('button')
-  await createButton.click()
+  await this.fillBookingForm('My Bookable To Be Deleted', 'NYC', 1)
+  await this.selectBookable('Black Room')
+  await this.submitBookingForm()
 })
 
 When('I am now editing details through the My Bookings page', async function() {
   await this.getWithLogin('/bookings')
 
+  // TODO: Curse these element transitions!
   await this.driver.sleep(1000)
 
   await this.findElementById('next').click()
-  const element = await this.waitUntilElement(By.partialLinkText('My Bookable To Be Deleted'))
-  await element.click()
+  await this.waitUntilElementByPartialLinkText('My Bookable To Be Deleted').click()
 })
 
 When('I click the Cancel Booking button', async function() {
-  const element = await this.waitUntilElement(By.linkText('Cancel Booking'))
-  await element.click()
+  await this.waitUntilElementByLinkText('Cancel Booking').click()
 })
 
 Then('It is cancelled', async function() {
--- test/e2e/features/support/world.js
+++ test/e2e/features/support/world.js
@@ -2,9 +2,15 @@ import { defineSupportCode, setWorldConstructor } from 'cucumber'
 
 import { By, Builder, until } from 'selenium-webdriver'
 import chrome from 'selenium-webdriver/chrome'
+
 import faker from 'faker'
+
 import jwt from 'jsonwebtoken'
+
+import format from 'date-fns/format'
+import addMinutes from 'date-fns/add_minutes'
 import addDays from 'date-fns/add_days'
+import addWeeks from 'date-fns/add_weeks'
 
 function makeValidToken() {
   return JSON.stringify(
@@ -18,13 +24,21 @@ function makeAnotherUsersValidToken() {
   )
 }
 
+function makeStartEnd(start, weeks = 0) {
+  const pattern = 'YYYY-MM-DDTHH:mm'
+  start = addWeeks(start, weeks)
+  return [ format(start, pattern), format(addMinutes(start, 1), pattern) ]
+}
+
 class _JWhutWorld {
   constructor({ attach, parameters }) {
     this.attach = attach
     this.parameters = parameters
 
     this.LOCALURL = process.env.ENDPOINT_URI || 'http://localhost:3001'
-    this.start = faker.date.between(new Date(), addDays(new Date(), 6))
+
+    // This is annoyingly critical for testing conflicting availability. Gah.
+    this._start = faker.date.between(new Date, addDays(new Date, 6))
 
     const options = new chrome.Options()
     options.addArguments('no-sandbox')
@@ -61,43 +75,117 @@ class _JWhutWorld {
 
   async waitUntilElementTextContains(selector, text, timeout) {
     const element = await this.waitUntilElement(selector, timeout)
-    const result = await this.driver.wait(until.elementTextContains(element, text), timeout)
-    return result
+    return this.driver.wait(until.elementTextContains(element, text), timeout)
   }
 
   async waitUntilElementIsVisible(selector, timeout) {
     const element = await this.waitUntilElement(selector, timeout)
-    const result = await this.driver.wait(until.elementIsVisible(element), timeout)
-    return result
+    return this.driver.wait(until.elementIsVisible(element), timeout)
+  }
+
+  async fillBookingForm(subject, location, weeks = 0) {
+    const [ start, end ] = makeStartEnd(this._start, weeks)
+
+    const form = await this.waitUntilElementByCss('form')
+
+    const $location = await form.findElement(By.name('locationId'))
+    const $subject = await form.findElement(By.name('subject'))
+    const $start = await form.findElement(By.name('start'))
+    const $end = await form.findElement(By.name('end'))
+
+    await $location.sendKeys(location)
+    await $subject.sendKeys(subject)
+
+    // TODO: Don't appear to need this "check" any more...
+    // await this.driver.wait(async () => (await $start.getAttribute('value')).includes('T'))
+    await $start.clear()
+    await $start.sendKeys(start)
+
+    await $end.clear()
+    await $end.sendKeys(end)
+  }
+
+  async viewBookables() {
+    await this.findElementByLinkText('Rooms').click()
+    // Grumble grumble... Why not opposite of `until.stalenessOf`?
+    await this.driver.sleep(1000)  // Delay next action because of animating transitions and selenium is dumb
+  }
+
+  // If a room (bookable) is not available for the given start and end, then
+  // the bookables list will render a bookable-list-item with an H2 with the
+  // name of the bookable and an adjacent P tag with a reason why the bookable
+  // cannot be selected.
+  // Bookables that _are_ available do not include the P tag in the
+  // bookable-list-item
+  async isBookableBooked(name) {
+    try {
+      await this.waitUntilElementByXpath(`//h2[contains(text(),"${name}")]/../p`, 2000)
+      return true
+    } catch (error) {
+      return false
+    }
+  }
+
+  async selectBookable(name) {
+    await this.viewBookables()
+    const bookablesList = await this.waitUntilElementByXpath('//*[starts-with(@class,"list-bookables-list")]')
+    await this.waitUntilElementByXpath(`//h2[contains(text(),"${name}")]`).click()
+    await this.driver.wait(until.stalenessOf(bookablesList), 2000, 'bookables list should have been removed from DOM before 2 seconds')
+  }
+
+  async submitBookingForm() {
+    const form = await this.waitUntilElementByCss('form')
+    await form.findElement(By.css('button')).click()
   }
 }
 
 const JWhutWorld = (...args) => {
-  const makeBySelector = (name) => {
-    const [ first, ...rest ] = name.replace('findElementBy', '')
-    return By[[ first.toLowerCase(), ...rest ].join('')]
+  const makeByLocator = (prefix, wrapFn) => (byType, locator) => {
+    const [ first, ...rest ] = byType.replace(prefix, '')
+    const byFn = By[[ first.toLowerCase(), ...rest ].join('')](locator)
+    return wrapFn ? wrapFn(byFn) : byFn
   }
+
+  const findElementByLocator = makeByLocator('findElementBy')
+  const waitUntilElementByLocator = makeByLocator('waitUntilElementBy', until.elementLocated)
 
   const world = new _JWhutWorld(...args)
 
   const handler = {
     get(target, propKey, receiver) {  // eslint-disable-line
       // If the property exists on the target, just call it
-      if (propKey in target.__proto__)
+      if (propKey in target.__proto__) {
         return (...args) => Reflect.apply(target[propKey], target, args)
+      }
 
-      // Wrap `driver.findElement` and `By.<selectorType>` - note that we do
-      // not mark the function as `async` - if we did, then we would not be
-      // able to chain calls like `findElementById('someId').click()`
-      if (propKey.startsWith('findElementBy'))
-        return selector => Reflect.apply(target.driver.findElement, target.driver, [ makeBySelector(propKey)(selector) ])
+      // Wrap a couple of element locator functions
+      // `By.<selectorType>` - note that we do not mark the function
+      // as `async` - if we did, then we would not be able to chain
+      // calls like `findElementById('someId').click()`
+      if (propKey.startsWith('waitUntilElementBy')) {
+        return (locator, timeout = 15000) => Reflect.apply(target.driver.wait, target.driver, [ waitUntilElementByLocator(propKey, locator), timeout ])
+      }
+
+      if (propKey.startsWith('findElementBy')) {
+        return locator => Reflect.apply(target.driver.findElement, target.driver, [ findElementByLocator(propKey, locator) ])
+      }
 
       // Prevent infinite recursion by calling get reflectively on the
       // target for any properties that aren't on its prototype
       return Reflect.get(target, propKey, receiver)
     },
   }
-  return new Proxy(world, handler)
+  const proxy = new Proxy(world, handler)
+
+  // Bind specific classmethods to the proxy so we can make use of
+  // the meta-methods defined within the proxy' handler
+  world.fillBookingForm = world.fillBookingForm.bind(proxy)
+  world.selectBookable = world.selectBookable.bind(proxy)
+  world.submitBookingForm = world.submitBookingForm.bind(proxy)
+  world.viewBookables = world.viewBookables.bind(proxy)
+  world.isBookableBooked = world.isBookableBooked.bind(proxy)
+
+  return proxy
 }
 
 defineSupportCode(function({ setDefaultTimeout }) {
