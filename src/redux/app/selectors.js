2a7b4393c9de749164873a1dbb9ded13b6bd994b
Lizzie Szoke <elizabeth.szoke@wipro.com>
2018-01-04 11:02:50 -0700

    BKIT-202 remove extra line

--- src/redux/app/selectors.js
+++ src/redux/app/selectors.js
@@ -1,5 +1,3 @@
 export const getSelectedLocation = state => state.app.get('selectedLocation', null)
 
 export const getRouterLocation = state => state.router.location
-
-export const getErrors = state => state.errors
